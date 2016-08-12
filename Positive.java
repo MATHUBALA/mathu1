d6a209bf3f44665cb5fe97beced63fa454b5cf03
MATHUBALA <mathubala.mcse@ksrce.ac.in>
2016-08-12 21:17:17 +0530

    Add files via upload

---
+++ Positive.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+public class Positive {
+	public static void main(String[] args) {
+		int a;
+		Scanner scanner=new Scanner(System.in);
+		a=scanner.nextInt();
+		if(a>=0){
+			System.out.println("The number is positive ");
+		}else{
+			System.out.println("The number is negative");
+		}
+
+	}
+
+}
