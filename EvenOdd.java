be115a23d8d517037367ac7f70548a6a09febde1
MATHUBALA <mathubala.mcse@ksrce.ac.in>
2016-08-12 21:24:54 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+
+public class EvenOdd {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	int a;
+	 Scanner scanner=new Scanner(System.in);
+	 a=scanner.nextInt();
+	 if(a%2==0){
+		 System.out.println("this number is even number");
+	 }else{
+		 System.out.println("This number is odd number");
+	 }
+	 
+	}
+
+}
