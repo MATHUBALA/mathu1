429c78594d204673e14684aff1fe10a2eea289a9
MATHUBALA <mathubala.mcse@ksrce.ac.in>
2017-07-21 02:31:53 -0700

    Create sumOfn.java

---
+++ sumOfn.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class SumOfNnum{
+ int n=0,sum=0;
+ Scanner s=new Scanner(System.in);
+ n=s.nextInt();
+   for(int i=0;i<n;i++){
+       sum=sum+i;
+       }
+  System.out.println(sum);
+  }
