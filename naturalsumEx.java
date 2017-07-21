1168f1651eb3a0a36958837659ace2b985f0e544
MATHUBALA <mathubala.mcse@ksrce.ac.in>
2017-07-20 23:33:44 -0700

    Rename naturalsum.java to naturalsumEx.java

---
+++ naturalsumEx.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public Class naturalsum{ 
+public static void main(Sring args[]){
+  int i,sum=0;
+  Scanner s=new Scanner(System.in);
+  x=s.nextInt();
+  while(i<=x){
+    sum=sum+i;
+    i++;
+    }
+    System.out.println("The natural number sum is="+sum);
+}
+}
