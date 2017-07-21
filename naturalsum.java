54b1fe272d5f34097838f47ee6cb87812d5e25a4
MATHUBALA <mathubala.mcse@ksrce.ac.in>
2017-07-21 00:00:33 -0700

    Update naturalsum.java

--- naturalsum.java
+++ naturalsum.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class naturalsum{ 
 public static void main(Sring args[]){
-  int i,sum=0;
+  int i,sum=0,x;
   Scanner s=new Scanner(System.in);
   x=s.nextInt();
   while(i<=x){
